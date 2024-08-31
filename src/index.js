50e0fc065ea4e755c1a6788f4b56e478d8188c09
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-08-31 23:39:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { startServer } from "./server.js";
+import { initMongoDB } from "./db/initMongoDB.js";
+
+const bootstrap = async () => {
+    await initMongoDB();
+    startServer();
+};
+
+bootstrap();
+
+
